f5c623a5ec9642ea8067a6817bb371c752432a6f
JTStark <joao.baraldi61@gmail.com>
2015-07-28 14:45:24 -0300

    Splash screen & app icons defined

---
+++ MovileUp15-07/MovileUp15-07/Controllers/FavoritesManager.swift
@@ -0,0 +1,21 @@
+//
+//  FavoritesManager.swift
+//  MovileUp15-07
+//
+//  Created by iOS on 7/28/15.
+//  Copyright (c) 2015 JTStark. All rights reserved.
+//
+
+import Foundation
+
+class FavoritesManager {
+    var favoritesIdentifiers: Set<Int> {
+    // ??
+    }
+    func addIdentifier(identifier: Int) {
+    // ??
+    }
+    func removeIdentifier(identifier: Int) {
+    // ??
+    }
+}
